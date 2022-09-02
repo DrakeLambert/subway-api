df261561e0baa6bdcc68d4f987f8aca8247f958f
Drake Lambert <drake-lambert@outlook.com>
2022-09-01 21:40:12 -0500

    get frequented stations endpoint

---
+++ src/Endpoints/GetFrequentedStationsEndpoint.cs
@@ -0,0 +1,35 @@
+using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace SubwayApi.Endpoints;
+
+public class GetFrequentedStationsEndpoint : EndpointBaseAsync
+    .WithoutRequest
+    .WithActionResult<IEnumerable<Station>>
+{
+    private readonly SubwayApiDbContext _db;
+
+    public GetFrequentedStationsEndpoint(SubwayApiDbContext db)
+    {
+        _db = db;
+    }
+
+    [Authorize]
+    [HttpGet("frequented-stations")]
+    public override async Task<ActionResult<IEnumerable<Station>>> HandleAsync(CancellationToken cancellationToken = default)
+    {
+        var currentUsername = User.Identity!.Name!;
+        var frequentedStations = await _db.FrequentedStations
+            .Where(frequentedStation => frequentedStation.Username == currentUsername)
+            .Join(
+                _db.Stations,
+                frequentedStation => frequentedStation.StationId,
+                station => station.Id,
+                (_, station) => station
+            ).ToListAsync(cancellationToken: cancellationToken);
+
+        return Ok(frequentedStations);
+    }
+}
